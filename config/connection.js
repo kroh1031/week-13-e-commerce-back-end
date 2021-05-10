b0bc5f28a893005222d8920f3dae15c7a24d81f4
Karen Roh <karenroh@hotmail.com>
2021-05-10 12:10:23 -0400

    able to seed data after configuring the path to the .env file in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
