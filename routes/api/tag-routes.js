c9d8f33ec07aeaec406476840abd8ff4f6e8ac43
Karen Roh <karenroh@hotmail.com>
2021-05-14 11:26:31 -0400

    able to create a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,11 +33,17 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
 });
 
